1a382d41923f07541daecf953197d798c3c28929
Nathan Chambers <nathan.m.chambers@gmail.com>
2017-05-14 20:42:28 +1000

    Added build param -N (What you see is what you get) fixed rope pbos.

--- tools/nchambers_build_mod.bat
+++ tools/nchambers_build_mod.bat
@@ -3,10 +3,10 @@ set projectDir="C:\Users\nchambers\projects\SOCOMD"
 set toolsDir=%projectDir%\tools
 set modOutput="C:\Program Files (x86)\Steam\steamapps\common\Arma 3\MODS_SOCOMD_DEV\@socomd_dev\addons"
 
-start /D %projectDir% makepbo.exe socomd_main %modOutput%
-start /D %projectDir% makepbo.exe -P socomd_asorvs %modOutput%
-start /D %projectDir% makepbo.exe -P socomd_zeus %modOutput%
-start /D %projectDir% makepbo.exe -P ar_advancedrappelling %modOutput%
-start /D %projectDir% makepbo.exe -P sa_advancedslingloading %modOutput%
+start /D %projectDir% makepbo.exe -N socomd_main %modOutput%
+start /D %projectDir% makepbo.exe -N -P socomd_asorvs %modOutput%
+start /D %projectDir% makepbo.exe -N -P socomd_zeus %modOutput%
+start /D %projectDir% makepbo.exe -N -P ar_advancedrappelling %modOutput%
+start /D %projectDir% makepbo.exe -N -P sa_advancedslingloading %modOutput%
 
 pause
