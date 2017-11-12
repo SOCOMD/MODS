4782a57cd59009187fc60aba92adf28f2a66b739
Nathan Chambers <nathan.m.chambers@gmail.com>
2017-11-13 10:00:47 +1100

    Updated build tool

--- tools/nchambers_build_mod.bat
+++ tools/nchambers_build_mod.bat
@@ -4,6 +4,7 @@ set toolsDir=%projectDir%\tools
 set modOutput="C:\Program Files (x86)\Steam\steamapps\common\Arma 3\MODS_SOCOMD_DEV\@socomd_dev\addons"
 
 start /D %projectDir% makepbo.exe -N socomd_main %modOutput%
+start /D %projectDir% makepbo.exe -N socomd_gcam %modOutput%
 start /D %projectDir% makepbo.exe -N -P socomd_asorvs %modOutput%
 start /D %projectDir% makepbo.exe -N -P socomd_zeus %modOutput%
 start /D %projectDir% makepbo.exe -N -P ar_advancedrappelling %modOutput%
