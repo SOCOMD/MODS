1dbfdcae0f9e82529119379f1e20bc35dfd137f8
Joseph Williams <seth.lives@gmail.com>
2017-12-28 01:59:22 +1030

    reference fixes

--- socomd_main/configs/vehicles/fixes.hpp
+++ socomd_main/configs/vehicles/fixes.hpp
@@ -254,12 +254,13 @@ class O_Pilot_F : O_helipilot_F
 	backpack = "ACE_NonSteerableParachute";
 };
 
+class B_Soldier_05_f;
 class B_Pilot_F : B_Soldier_05_f
 {
 	backpack = "ACE_NonSteerableParachute";
-	modelsides[] = {3,2,1,0};
 };
 
+class I_Soldier_04_F;
 class I_pilot_F : I_Soldier_04_F
 {
 	backpack = "ACE_NonSteerableParachute";
