3cbcaf2fa40e86f70b39cda496c5c31441b742dd
Nathan Chambers <nathan.m.chambers@gmail.com>
2017-12-28 01:15:11 +1100

    Modified 2CDO groups. Split x/y position of units, added pilot group and homestead group.

--- socomd_main/configs/CfgGroups.hpp
+++ socomd_main/configs/CfgGroups.hpp
@@ -1,10 +1,10 @@
-#define DEFINE_UNIT(idx,type,rating) \
-class Unit##idx \
+#define DEFINE_UNIT(idxX,idxY,type,rating) \
+class Unit##idxX##idxY \
 { \
 	side = 1; \
 	vehicle = type; \
 	rank = rating; \
-	position[] = {0, -(idx * 2) ,0}; \
+	position[] = {(idxX * 1), -(idxY * 1) ,0}; \
 };
 
 class CfgGroups
@@ -27,10 +27,10 @@ class CfgGroups
 					side = 1;
 					rarityGroup = 0.75;
 
-					DEFINE_UNIT(0, "SOCOMD_PlatoonLeader", "LIEUTENANT")
-					DEFINE_UNIT(1, "SOCOMD_PlatoonSergeant", "LIEUTENANT")
-					DEFINE_UNIT(2, "SOCOMD_PlatoonSignaller", "CORPORAL")
-					DEFINE_UNIT(3, "SOCOMD_PlatoonMedic", "CORPORAL")
+					DEFINE_UNIT(0,0,"SOCOMD_PlatoonLeader", "LIEUTENANT")
+					DEFINE_UNIT(0,1,"SOCOMD_PlatoonSergeant", "LIEUTENANT")
+					DEFINE_UNIT(0,2,"SOCOMD_PlatoonSignaller", "CORPORAL")
+					DEFINE_UNIT(0,3,"SOCOMD_PlatoonMedic", "CORPORAL")
 				};
 
 				class SOCOMD_CMD_Section
@@ -40,14 +40,14 @@ class CfgGroups
 					side = 1;
 					rarityGroup = 0.75;
 
-					DEFINE_UNIT(0, "SOCOMD_SectionLeader", "SERGEANT")
-					DEFINE_UNIT(1, "SOCOMD_2IC", "CORPORAL")
-					DEFINE_UNIT(2, "SOCOMD_Sapper", "PRIVATE")
-					DEFINE_UNIT(3, "SOCOMD_Gunner", "PRIVATE")
-					DEFINE_UNIT(4, "SOCOMD_Rifleman", "PRIVATE")
-					DEFINE_UNIT(5, "SOCOMD_Marksman", "PRIVATE")
-					DEFINE_UNIT(6, "SOCOMD_AT", "PRIVATE")
-					DEFINE_UNIT(7, "SOCOMD_Medic", "PRIVATE")
+					DEFINE_UNIT(0,0,"SOCOMD_SectionLeader", "SERGEANT")
+					DEFINE_UNIT(0,1,"SOCOMD_2IC", "CORPORAL")
+					DEFINE_UNIT(0,2,"SOCOMD_Sapper", "PRIVATE")
+					DEFINE_UNIT(0,3,"SOCOMD_Gunner", "PRIVATE")
+					DEFINE_UNIT(1,0,"SOCOMD_Rifleman", "PRIVATE")
+					DEFINE_UNIT(1,1,"SOCOMD_Marksman", "PRIVATE")
+					DEFINE_UNIT(1,2,"SOCOMD_AT", "PRIVATE")
+					DEFINE_UNIT(1,3,"SOCOMD_Medic", "PRIVATE")
 				};
 
 				class SOCOMD_CMD_Sniper_Team
@@ -57,8 +57,8 @@ class CfgGroups
 					side = 1;
 					rarityGroup = 0.75;
 
-					DEFINE_UNIT(0, "SOCOMD_Recon_TeamLeader", "SERGEANT")
-					DEFINE_UNIT(1, "SOCOMD_Sniper_Spotter", "CORPORAL")
+					DEFINE_UNIT(0,0,"SOCOMD_Recon_TeamLeader", "SERGEANT")
+					DEFINE_UNIT(0,1,"SOCOMD_Sniper_Spotter", "CORPORAL")
 				};
 
 				class SOCOMD_CMD_Weapons_Team
@@ -68,9 +68,33 @@ class CfgGroups
 					side = 1;
 					rarityGroup = 0.75;
 
-					DEFINE_UNIT(0, "SOCOMD_Weapons_TeamLeader", "CORPORAL")
-					DEFINE_UNIT(1, "SOCOMD_Weapons_Specialist", "PRIVATE")
-					DEFINE_UNIT(2, "SOCOMD_Weapons_Assistant", "PRIVATE")
+					DEFINE_UNIT(0,0,"SOCOMD_Weapons_TeamLeader", "CORPORAL")
+					DEFINE_UNIT(0,1,"SOCOMD_Weapons_Specialist", "PRIVATE")
+					DEFINE_UNIT(0,2,"SOCOMD_Weapons_Assistant", "PRIVATE")
+				};
+
+				class SOCOMD_171_Pilots
+				{
+					name = "171 Aviation Pilots";
+					faction = "SOCOMD_CMD";
+					side = 1;
+					rarityGroup = 0.75;
+
+					DEFINE_UNIT(0,0,"SOCOMD_Pilot", "CORPORAL")
+					DEFINE_UNIT(0,1,"SOCOMD_Pilot", "CORPORAL")
+				};
+
+				class SOCOMD_CMD_Homesteads
+				{
+					name = "Homesteads";
+					faction = "SOCOMD_CMD";
+					side = 1;
+					rarityGroup = 0.75;
+
+					DEFINE_UNIT(0,0,"SOCOMD_Homestead", "PRIVATE")
+					DEFINE_UNIT(0,1,"SOCOMD_Homestead", "PRIVATE")
+					DEFINE_UNIT(0,2,"SOCOMD_Homestead", "PRIVATE")
+					DEFINE_UNIT(0,3,"SOCOMD_Homestead", "PRIVATE")
 				};
 			};
 
@@ -86,10 +110,10 @@ class CfgGroups
 					side = 1;
 					rarityGroup = 0.75;
 
-					DEFINE_UNIT(0, "SOCOMD_SASR_Trooper", "CORPORAL")
-					DEFINE_UNIT(1, "SOCOMD_SASR_Trooper", "PRIVATE")
-					DEFINE_UNIT(2, "SOCOMD_SASR_Trooper", "PRIVATE")
-					DEFINE_UNIT(3, "SOCOMD_SASR_Trooper", "PRIVATE")
+					DEFINE_UNIT(0,0,"SOCOMD_SASR_Trooper", "CORPORAL")
+					DEFINE_UNIT(0,1,"SOCOMD_SASR_Trooper", "PRIVATE")
+					DEFINE_UNIT(0,2,"SOCOMD_SASR_Trooper", "PRIVATE")
+					DEFINE_UNIT(0,3,"SOCOMD_SASR_Trooper", "PRIVATE")
 				};
 
 				class SOCOMD_SASR_TROOP_PATROL
@@ -99,12 +123,12 @@ class CfgGroups
 					side = 1;
 					rarityGroup = 0.75;
 
-					DEFINE_UNIT(0, "SOCOMD_SASR_Trooper", "CORPORAL")
-					DEFINE_UNIT(1, "SOCOMD_SASR_Trooper", "PRIVATE")
-					DEFINE_UNIT(2, "SOCOMD_SASR_Trooper", "PRIVATE")
-					DEFINE_UNIT(3, "SOCOMD_SASR_Trooper", "PRIVATE")
-					DEFINE_UNIT(4, "SOCOMD_SASR_Trooper", "PRIVATE")
-					DEFINE_UNIT(5, "SOCOMD_SASR_Trooper", "PRIVATE")
+					DEFINE_UNIT(0,0,"SOCOMD_SASR_Trooper", "CORPORAL")
+					DEFINE_UNIT(0,1,"SOCOMD_SASR_Trooper", "PRIVATE")
+					DEFINE_UNIT(0,2,"SOCOMD_SASR_Trooper", "PRIVATE")
+					DEFINE_UNIT(0,3,"SOCOMD_SASR_Trooper", "PRIVATE")
+					DEFINE_UNIT(0,4,"SOCOMD_SASR_Trooper", "PRIVATE")
+					DEFINE_UNIT(0,5,"SOCOMD_SASR_Trooper", "PRIVATE")
 				};
 			};
 		};
