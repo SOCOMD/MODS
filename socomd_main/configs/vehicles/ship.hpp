b6fff4b1701c996c3de38aa9e5bcaf50c393d15d
Nathan Chambers <nathan.m.chambers@gmail.com>
2018-07-10 14:39:51 +1000

    Tuned socomd boat performance.

--- socomd_main/configs/vehicles/ship.hpp
+++ socomd_main/configs/vehicles/ship.hpp
@@ -45,6 +45,28 @@ class SOCOMD_GUNBOAT : CUP_B_RHIB_USMC
 	crew = "SOCOMD_Medic";
 	typicalCargo[] = {"SOCOMD_Medic"};
 
+	//Performance
+	simulation = "shipX";
+	idleRpm = 100;
+	redRpm = 1000;
+	thrustDelay = 0.5;
+	enginePower = 200;
+	engineShiftY = 0;
+	waterSpeedFactor = 1;
+	waterResistanceCoef = 0.005;
+	waterAngularDampingCoef = 1.75;
+	waterLinearDampingCoefX = 15.0;
+	waterLinearDampingCoefY = 1.2;
+	rudderForceCoef = 0.5;
+	rudderForceCoefAtMaxSpeed = 0.0045;
+	waterLeakiness = 5;
+	waterEffectSpeed = 5;
+	waterFastEffectSpeed = 25;
+	overSpeedBrakeCoef = 0.8;
+
+	minSpeed = -0.5;
+	maxSpeed = 65;
+
 	class TransportMagazines 
 	{
 		LandTransportMagazines
