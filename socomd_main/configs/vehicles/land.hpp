7ec4dacdd5389b61bd767b11e6af2276d1932bc6
Nathan Chambers <nathan.m.chambers@gmail.com>
2018-08-16 15:32:57 +1000

    Swapped textures from woodland to desert on desert jackal

--- socomd_main/configs/vehicles/land.hpp
+++ socomd_main/configs/vehicles/land.hpp
@@ -308,9 +308,9 @@ class SOCOMD_JACKAL_HMG_D : CUP_B_Jackal2_L2A1_GB_D
 
 	hiddenSelections[] = {"camo1","camo2","camo3"};
 	hiddenSelectionsTextures[] = {
-		"\CUP\WheeledVehicles\CUP_WheeledVehicles_Jackal\data\jackal_base_woodlnd_co.paa",
-		"\CUP\WheeledVehicles\CUP_WheeledVehicles_Jackal\data\jackal_mount_woodlnd_co.paa",
-		"socomd_data\Nary\jackal_adds_co.paa",
+		"\CUP\WheeledVehicles\CUP_WheeledVehicles_Jackal\data\jackal_base_co.paa",
+		"\CUP\WheeledVehicles\CUP_WheeledVehicles_Jackal\data\jackal_mount_co.paa",
+		"socomd_data\Nary\jackal_adds_co.paa"
 	};
 
 	/*tf_hasLRradio = 1;
