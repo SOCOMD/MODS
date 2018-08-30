70f0a6cc783290670c0d9b4d2368be1bf2ca9010
Nathan Chambers <nathan.m.chambers@gmail.com>
2018-08-30 10:27:51 +1000

    Updated addon version v1.0.2

--- socomd_main/config.cpp
+++ socomd_main/config.cpp
@@ -5,9 +5,9 @@ class CfgPatches
 	class socomd_main 
 	{
 		//Internal Version
-		version = 1.0.1;
-		versionStr = "1.0.1";
-		versionAr[] = { 1,0,1 };
+		version = 1.0.2;
+		versionStr = "1.0.2";
+		versionAr[] = { 1,0,2 };
 
 		//Required Version
 		requiredVersion = 0.1;
