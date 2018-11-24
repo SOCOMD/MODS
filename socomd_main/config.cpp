73d8ef3afffe4075679d5eb109726b1951b7521b
Nathan Chambers <nathan.m.chambers@gmail.com>
2018-11-24 19:29:20 +1100

    Updated main PBO version 1.0.4

--- socomd_main/config.cpp
+++ socomd_main/config.cpp
@@ -5,9 +5,9 @@ class CfgPatches
 	class socomd_main 
 	{
 		//Internal Version
-		version = 1.0.3;
-		versionStr = "1.0.3";
-		versionAr[] = { 1,0,3 };
+		version = 1.0.4;
+		versionStr = "1.0.4";
+		versionAr[] = { 1,0,4 };
 
 		//Required Version
 		requiredVersion = 0.1;
