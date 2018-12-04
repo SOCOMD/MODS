bd23fc164fe03f6bad99cbdedefda0a08d998df6
Anthony "Zanven" Poschen <zanven42@gmail.com>
2018-12-04 23:37:19 +1100

    sorted land vehicles pass 1

--- socomd_core/predefined.hpp
+++ socomd_core/predefined.hpp
@@ -18,6 +18,7 @@
 #define AUTHOR_STR "SOCOMD Mod"
 #define FACTION_STR "SOCOMD_CMD"
 #define SOCOMD_PILOT "SOCOMD_Pilot"
+#define SOCOMD_MEDIC "SOCOMD_Medic"
 
 //#define true			1
 //#define false			0
