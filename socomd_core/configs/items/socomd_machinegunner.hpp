aa89b8dacce08a87cd780e289001c13d729a346f
Anthony Poschen <zanven42@gmail.com>
2018-12-08 02:35:18 +1100

    added config for some loadouts base items

---
+++ socomd_core/configs/items/socomd_machinegunner.hpp
@@ -0,0 +1,29 @@
+class sma_minimi_mk3_762tsb;
+class SOCOMD_Item_Primary_MachineGunner : sma_minimi_mk3_762tsb;
+
+class SOCOMD_Item_Primary_Magazine_MachineGunner;
+
+class DSO_hgun_Pistol_heavy_01_blk_F;
+class SOCOMD_Item_Handgun_MachineGunner : DSO_hgun_Pistol_heavy_01_blk_F;
+
+class 11Rnd_45ACP_Mag;
+class SOCOMD_Item_Handgun_Magazine_MachineGunner : 11Rnd_45ACP_Mag;
+
+class SOCOMD_Item_Secondary_MachineGunner;
+
+class ADFU_H_Airframe_Cover_03_MC;
+class SOCOMD_Item_Headgear_MachineGunner : ADFU_H_Airframe_Cover_03_MC;
+
+class SOCOMD_Item_Binocular_MachineGunner;
+
+class ItemMicroDAGR;
+class SOCOMD_Item_GPS_MachineGunner : ItemMicroDAGR;
+
+class SOCOMD_Commando_SleevesDown;
+class SOCOMD_Item_Uniform_MachineGunner : SOCOMD_Commando_SleevesDown;
+
+class VSM_LBT6094_MG_Multicam;
+class SOCOMD_Item_Vest_MachineGunner : VSM_LBT6094_MG_Multicam;
+
+class SOG_BAG_MC;
+class SOCOMD_Item_Backpack_MachineGunner : SOG_BAG_MC;
---
+++ socomd_core/configs/items/socomd_rifleman.hpp
@@ -0,0 +1,31 @@
+class SMA_M4afgSTOCK;
+class SOCOMD_Item_Primary_Rifleman : SMA_M4afgSTOCK;
+
+class SMA_30Rnd_556x45_M855A1;
+class SOCOMD_Item_Primary_Magazine_Rifleman : SMA_30Rnd_556x45_M855A1;
+
+class DSO_hgun_Pistol_heavy_01_blk_F;
+class SOCOMD_Item_Handgun_Rifleman : DSO_hgun_Pistol_heavy_01_blk_F;
+
+class 11Rnd_45ACP_Mag;
+class SOCOMD_Item_Handgun_Magazine_Rifleman : 11Rnd_45ACP_Mag;
+
+class SOCOMD_Item_Secondary_Rifleman;
+
+class ADFU_H_Airframe_Cover_03_MC;
+class SOCOMD_Item_Headgear_Rifleman : ADFU_H_Airframe_Cover_03_MC;
+
+class Binocular;
+class SOCOMD_Item_Binocular_Rifleman : Binocular;
+
+class ItemMicroDAGR;
+class SOCOMD_Item_GPS_Rifleman : ItemMicroDAGR;
+
+class SOCOMD_Commando_SleevesDown;
+class SOCOMD_Item_Uniform_Rifleman : SOCOMD_Commando_SleevesDown;
+
+class SOCOMD_Operator_Vest;
+class SOCOMD_Item_Vest_Rifleman : SOCOMD_Operator_Vest;
+
+class SOG_BAG_MC;
+class SOCOMD_Item_Backpack_Rifleman : SOG_BAG_MC;
