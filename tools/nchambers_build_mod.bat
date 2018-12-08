1c31955ab986521356367a9d06fbb411ba1fe33f
Nathan Chambers <nathan.m.chambers@gmail.com>
2018-12-09 03:48:40 +1100

    Renamed PBO prefix.

--- tools/nchambers_build_mod.bat
+++ tools/nchambers_build_mod.bat
@@ -2,7 +2,7 @@
 set projectDir="C:\Users\nchambers\projects\SOCOMD\mods"
 set toolsDir=%projectDir%\tools
 set modOutput="C:\Program Files (x86)\Steam\steamapps\common\Arma 3\SOCOMD_Dev\@socomd_dev\addons"
-set modOutput_lite="C:\Program Files (x86)\Steam\steamapps\common\Arma 3\SOCOMD_Dev\@socomd_lite\addons"
+set modOutput_lite="C:\Program Files (x86)\Steam\steamapps\common\Arma 3\SOCOMD_Mods\SOCOMD_Dev\@socomd_core\addons"
 set modOutput_cup_compat="C:\Program Files (x86)\Steam\steamapps\common\Arma 3\SOCOMD_Dev\@socomd_cup_compat\addons"
 set modOutput_rhs_compat="C:\Program Files (x86)\Steam\steamapps\common\Arma 3\SOCOMD_Dev\@socomd_rhs_compat\addons"
 
