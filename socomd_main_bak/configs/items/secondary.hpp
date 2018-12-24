83ee2910396f0068cce3ee1f27067715ef6d3b42
Nathan Chambers <nathan.m.chambers@gmail.com>
2018-12-24 21:42:03 +1100

    Deleting old files.

--- socomd_main_bak/configs/items/secondary.hpp
+++
@@ -1,7 +0,0 @@
-class HAFM_LAW;
-class CUP_launch_Javelin;
-class tf47_m3maaws;
-class SOCOMD_Carl_Gustav : tf47_m3maaws {
-	author = AUTHOR_STR;
-	displayName = "Carl Gustav";
-};
--- socomd_main_bak/functions/common/fn_CommonInitServer.sqf
+++
@@ -1,4 +0,0 @@
-if(!isServer) exitWith {};
-
-//Call CommonInitLocal on all connected clients/JIP clients
-[[], "SOCOMD_fnc_CommonInitLocal", true, true, true] call BIS_fnc_MP;
