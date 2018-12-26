1390b70e02a7aaf32820bd731d2f96842543a7b0
Anthony "Zanven" Poschen <zanven42@gmail.com>
2018-12-26 19:02:35 +1100

    updated monks build tools

--- tools/monk_build_mod.bat
+++ tools/monk_build_mod.bat
@@ -1,13 +1,15 @@
 @echo off
 set projectDir="C:\git\socomd"
 set toolsDir=%projectDir%\tools
-set modOutput="C:\Program Files (x86)\Steam\steamapps\common\Arma 3\SOCOMD_Dev\@socomd_dev\addons"
+set modOutput_core="C:\Program Files (x86)\Steam\steamapps\common\Arma 3\SOCOMD_Mods\SOCOMD_Core\@socomd_core\addons"
+set modOutput_main="C:\Program Files (x86)\Steam\steamapps\common\Arma 3\SOCOMD_Mods\SOCOMD_Core\@socomd_main\addons"
 
-start /D %projectDir% makepbo.exe -N -P socomd_main %modOutput%
-start /D %projectDir% makepbo.exe -N -P socomd_gcam %modOutput%
-start /D %projectDir% makepbo.exe -N -P socomd_asorvs %modOutput%
-start /D %projectDir% makepbo.exe -N -P socomd_asorgs %modOutput%
-start /D %projectDir% makepbo.exe -N -P socomd_zeus %modOutput%
-start /D %projectDir% makepbo.exe -N -P socomd_data %modOutput%
-start /D %projectDir% makepbo.exe -N -P ar_advancedrappelling %modOutput%
-start /D %projectDir% makepbo.exe -N -P sa_advancedslingloading %modOutput%
+start /D %projectDir% makepbo.exe -N -P socomd_main %modOutput_core%
+start /D %projectDir% makepbo.exe -N -P socomd_main %modOutput_main%
+# start /D %projectDir% makepbo.exe -N -P socomd_gcam %modOutput%
+# start /D %projectDir% makepbo.exe -N -P socomd_asorvs %modOutput%
+# start /D %projectDir% makepbo.exe -N -P socomd_asorgs %modOutput%
+# start /D %projectDir% makepbo.exe -N -P socomd_zeus %modOutput%
+# start /D %projectDir% makepbo.exe -N -P socomd_data %modOutput%
+# start /D %projectDir% makepbo.exe -N -P ar_advancedrappelling %modOutput%
+# start /D %projectDir% makepbo.exe -N -P sa_advancedslingloading %modOutput%
