5bc9ef7ee5ab50f482d5c78b8773a9f887283289
Anthony "Zanven" Poschen <zanven42@gmail.com>
2018-12-26 15:42:54 +1100

    duplicated predefines

--- socomd_main/configs/loadouts/default.hpp
+++ socomd_main/configs/loadouts/default.hpp
@@ -1,6 +1,27 @@
+#define LOADOUT_VEST_ESSENTIALS \
+	LOADOUT_ITEM(CUP_HandGrenade_M67, 2) \
+	LOADOUT_ITEM(ACE_M84, 2) \
+	LOADOUT_ITEM(SmokeShell, 5) \
+	LOADOUT_ITEM(ACRE_PRC343, 1) \
+	LOADOUT_ITEM(ACE_IR_Strobe_Item, 1)
+
+class ACE_IR_Strobe_Item;
+class ACRE_PRC343;
+class SmokeShell;
+class ACE_M84;
+class CUP_HandGrenade_M67;
+class ACE_Kestrel4500;
+class ACE_RangeCard;
+class SOCOMD_Item_Magazine_556x45_30Rnd;
 ////////////////////////////////////////////////////////////////////////////////
 // Sniper
-class SOCOMD_SNIPER;
+class Inventory;
+class Vest {
+	class Inventory;
+};
+class SOCOMD_SNIPER {
+	class Vest;
+};
 class SOCOMD_SNIPER: SOCOMD_SNIPER {
 	class Vest {
 		type = ITEM_VEST_SNIPER;
