3481eb15a2ec7f2ff6ab4c0b0e877af7f22f5f83
Anthony Poschen <anthonyposchen@protonmail.com>
2018-12-29 18:19:55 +1100

    attempt at working out if you can put vanilla weapons in the backpack.

--- socomd_core/configs/weapons/base/primary.hpp
+++ socomd_core/configs/weapons/base/primary.hpp
@@ -4,12 +4,16 @@ class arifle_SPAR_01_blk_F; //Using CBA_556x45_STANA
 class LMG_03_F; //Using CBA_556x45_MINIMI
 class arifle_SPAR_03_blk_F; //Using CBA_762x51_HK417
 
-class LRR_base_F;
+class LRR_base_F {
+	class WeaponSlotsInfo;
+};
 //Using SOCOMD_408_MAG
 class srifle_LRR_F : LRR_base_F {
 	magazineWell[] = {"SOCOMD_408_MAG"};
-	mass=60;
-	allowedSlots[]={901}; // 701 = vest, 801 = uniform, 901 = backpack
+	class WeaponSlotsInfo : WeaponSlotsInfo {
+		mass=4;
+		allowedSlots[]={901}; // 701 = vest, 801 = uniform, 901 = backpack
+	};
 };
 
 class SMG_05_base_F;
