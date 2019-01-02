3fe53d6744e519be3104002046e7e9ce74747d90
Anthony Poschen <anthonyposchen@protonmail.com>
2019-01-02 11:41:40 +1100

    deciated offroad config, uses base offroads gun and ammo.

---
+++ socomd_cup_compat/configs/vehicles/override/offroad_armed.hpp
@@ -0,0 +1,54 @@
+class O_CUPOTban_Offroad_Armed_01 : O_G_Offroad_01_armed_F_OCimport_02 {
+    class Turrets : Turrets {
+        class M2_Turret : M2_Turret {
+            // yay a vanilla gun :D
+            weapons[] = {"HMG_M2"};
+            magazines[] = {
+            "500Rnd_127x99_mag_Tracer_Yellow",
+            "500Rnd_127x99_mag_Tracer_Yellow",
+            "500Rnd_127x99_mag_Tracer_Yellow",
+            "500Rnd_127x99_mag_Tracer_Yellow",
+            "500Rnd_127x99_mag_Tracer_Yellow",
+            "500Rnd_127x99_mag_Tracer_Yellow",
+            "500Rnd_127x99_mag_Tracer_Yellow",
+            "500Rnd_127x99_mag_Tracer_Yellow",
+            "500Rnd_127x99_mag_Tracer_Yellow",
+            "500Rnd_127x99_mag_Tracer_Yellow",
+            "500Rnd_127x99_mag_Tracer_Yellow",
+            "500Rnd_127x99_mag_Tracer_Yellow",
+            "500Rnd_127x99_mag_Tracer_Yellow",
+            "500Rnd_127x99_mag_Tracer_Yellow",
+            "500Rnd_127x99_mag_Tracer_Yellow",
+            "500Rnd_127x99_mag_Tracer_Yellow"
+            };
+        };
+    };
+};
+
+// asians
+class O_CUPOMEINS_Offroad_Armed_01 : O_G_Offroad_01_armed_F_OCimport_02 {
+    class Turrets : Turrets {
+        class M2_Turret : M2_Turret {
+            // yay a vanilla gun :D
+            weapons[] = {"HMG_M2"};
+            magazines[] = {
+            "500Rnd_127x99_mag_Tracer_Yellow",
+            "500Rnd_127x99_mag_Tracer_Yellow",
+            "500Rnd_127x99_mag_Tracer_Yellow",
+            "500Rnd_127x99_mag_Tracer_Yellow",
+            "500Rnd_127x99_mag_Tracer_Yellow",
+            "500Rnd_127x99_mag_Tracer_Yellow",
+            "500Rnd_127x99_mag_Tracer_Yellow",
+            "500Rnd_127x99_mag_Tracer_Yellow",
+            "500Rnd_127x99_mag_Tracer_Yellow",
+            "500Rnd_127x99_mag_Tracer_Yellow",
+            "500Rnd_127x99_mag_Tracer_Yellow",
+            "500Rnd_127x99_mag_Tracer_Yellow",
+            "500Rnd_127x99_mag_Tracer_Yellow",
+            "500Rnd_127x99_mag_Tracer_Yellow",
+            "500Rnd_127x99_mag_Tracer_Yellow",
+            "500Rnd_127x99_mag_Tracer_Yellow"
+            };
+        };
+    };
+};
