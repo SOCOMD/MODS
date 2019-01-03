c7dfab53477a67c7693406e9ded85105fbd89ca3
Anthony Poschen <anthonyposchen@protonmail.com>
2019-01-04 05:28:20 +1100

    the warrior now survives 1 RPG/ missile but will crash land. (may need to make main rotor survive).
    tweaking needed to get model looking rekt on crash landing.

--- compats/socomd_compat_cup/configs/vehicles/override/warrior.hpp
+++ compats/socomd_compat_cup/configs/vehicles/override/warrior.hpp
@@ -1,10 +1,239 @@
+// modify damage model of warrior
+class CUP_B_UH60M_US : CUP_Uh60_Base {
+	class HitPoints : Hitpoints {
+		class HitHull {
+			armor = 2.1;// default 1
+			material = -1;
+			// for some reason this set to 1 gives perfectly desired missile protection.
+			// survives 1 hit, second one blows it up. good luck landing mr pilot with no main rotor
+			explosionShielding = 1;
+			name = "trup";
+			passThrough = 1;
+			visual = "trup";
+		};
+		class HitAvionics : HitAvionics {
+			material = -1;// default 51
+		};
+		class HitEngine {
+			// 0.05 armor = 9 shots with 12.7mm to disable
+			armor = 0.05; // default 0.5
+			material = -1;// default 51
+			name = "motor";
+			passThrough = 1;
+			visual = "motor";
+			explosionShielding = 1.0;
+		};
+		class HitEngine1 : HitEngine {
+			material = -1;// default 51
+			name = "engine_1_hit";
+			convexComponent = "engine_1_hit";
+			// armor = 0.25;
+			// passThrough = 1;
+		};
+		class HitEngine2 : HitEngine {
+			material = -1;// default 51
+			name = "engine_2_hit";
+			convexComponent = "engine_2_hit";
+			// armor = 0.25;
+			// passThrough = 1;
+		};
+		class HitEngine3 : HitEngine {
+			material = -1;// default 51
+			name = "engine_3_hit";
+			convexComponent = "engine_3_hit";
+			// armor = 0.25;
+			// passThrough = 1;
+		};
+		class HitFuel : HitHull {
+			convexComponent = "fuel_hit";
+			name = "fuel_hit";
+			material = -1;
+			// armor = 1;
+			// passThrough = 1;
+		};
+		class HitGear {
+			armor = 0.9;
+			material = -1;
+			name = "gear";
+			passThrough = 0;
+		};
+		class HitGlass1 {
+			armor = 2;
+			material = -1;
+			name = "glass1";
+			passThrough = 0;
+			visual = "glass1";
+		};
+		class HitGlass2 {
+			armor = 2;
+			material = -1;
+			name = "glass2";
+			passThrough = 0;
+			visual = "glass2";
+		};
+		class HitGlass3 {
+			armor = 2;
+			material = -1;
+			name = "glass3";
+			passThrough = 0;
+			visual = "glass3";
+		};
+		class HitGlass4 {
+			armor = 2;
+			material = -1;
+			name = "glass4";
+			passThrough = 0;
+			visual = "glass4";
+		};
+		class HitGlass5 {
+			armor = 2;
+			material = -1;
+			name = "glass5";
+			passThrough = 0;
+			visual = "glass5";
+		};
+		class HitGlass6 {
+			armor = 2;
+			material = -1;
+			name = "glass6";
+			passThrough = 0;
+			visual = "glass6";
+		};
+		class HitHRotor {
+			armor = 1; // default 0.5
+			material = -1;// default 51
+			name = "velka vrtule";
+			passThrough = 0.2; // default 0.1
+			visual = "velka vrtule staticka";
+		};
+		class HitHStabilizerL1 {
+			armor = 0.8;
+			material = -1;
+			name = "HStabilizerL1";
+			passThrough = 1;
+		};
+		class HitHStabilizerR1 {
+			armor = 0.8;
+			material = -1;
+			name = "HStabilizerR1";
+			passThrough = 1;
+		};
+		class HitHydraulics {
+			armor = 0.8;
+			material = -1;
+			name = "hydraulics";
+			passThrough = 0.8;
+		};
+		class HitLGlass {
+			armor = 0.1;
+			convexComponent = "sklo predni L";
+			explosionShielding = 1;
+			material = -1;// default 51
+			name = "sklo predni L";
+			passThrough = 0;
+			visual = "sklo predni L";
+		};
+		class HitLight {
+			armor = 0.1;
+			material = -1;
+			name = "light";
+			passThrough = 0;
+		};
+		class HitMissiles : HitMissiles {
+			armor = 0.1;
+			convexComponent = "ammo_hit";
+			explosionShielding = 1;
+			material = -1;// default 51
+			name = "ammo_hit";
+			passThrough = 0.5;
+			visual = "munice";
+		};
+		class HitPitotTube {
+			armor = 0.5;
+			material = -1;
+			name = "pitot tube";
+			passThrough = 0.2;
+		};
+		class HitRGlass {
+			armor = 0.1;
+			convexComponent = "sklo predni P";
+			explosionShielding = 1;
+			material = -1;// default 51
+			name = "sklo predni P";
+			passThrough = 0;
+			visual = "sklo predni P";
+		};
+		class HitStarter1 {
+			armor = 0.1;
+			material = -1;
+			name = "starter1";
+			passThrough = 0;
+		};
+		class HitStarter2 {
+			armor = 0.1;
+			material = -1;
+			name = "starter2";
+			passThrough = 0;
+		};
+		class HitStarter3 {
+			armor = 0.1;
+			material = -1;
+			name = "starter3";
+			passThrough = 0;
+		};
+		class HitStaticPort {
+			armor = 0.1;
+			material = -1;
+			name = "static port";
+			passThrough = 1;
+		};
+		class HitTail {
+			armor = 0.8;
+			material = -1;
+			name = "tail boom";
+			explosionShielding = 1.0;
+			passThrough = 0.8;
+		};
+		class HitTransmission {
+			armor = 0.8;
+			material = -1;
+			name = "transmission";
+			passThrough = 0.8;
+			explosionShielding = 1.0;
+		};
+		class HitVRotor {
+			armor = 0.5;
+			material = -1;// default 51
+			name = "mala vrtule";
+			passThrough = 0.3;
+			explosionShielding = 1.0;
+			visual = "mala vrtule staticka";
+		};
+		class HitVStabalizer1 {
+			armor = 0.8;
+			material = -1;
+			name = "VStabilizer1";
+			explosionShielding = 1.0;
+			passThrough = 1;
+		};
+		class HitWinch {
+			armor = -40;
+			material = -1;// default 51
+			name = "slingLoad0";
+			passThrough = 0;
+			radius = 0.1;
+			visual = "";
+		};
+	};
+};
+
+
 /*
 CUP_B_UH60M_US <== TARGET
 CUP_Uh60_Base
 Helicopter_Base_H
 */
 
-class CUP_B_UH60M_US;
 class SOCOMD_WARRIOR_A : CUP_B_UH60M_US {
 	scope = public;
 	scopeCurator = public;
