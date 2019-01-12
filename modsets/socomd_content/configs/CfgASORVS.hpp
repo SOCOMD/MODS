4b7c030938f5be17206af5f97c478d13a1b90179
Daniel S <dansgeo@gmail.com>
2019-01-12 18:59:58 +1000

    ASORVS fix

---
+++ modsets/socomd_content/configs/CfgASORVS.hpp
@@ -0,0 +1,24 @@
+class CfgASORVS {
+	class Rotary {
+		SOCOMD_Content[] = {
+		};
+	};
+
+	class Land {
+		SOCOMD_Content[] = {
+			"SOCOMD_SOV_HMG_D", 
+			"SOCOMD_SOV_HMG_W", 
+			"SOCOMD_LR_ATV"
+		};
+	};
+
+	class Sea {
+		SOCOMD_Content[] = {
+		};
+	};
+
+	class Supply {
+		SOCOMD_Content[] = {
+		};
+	};
+};
