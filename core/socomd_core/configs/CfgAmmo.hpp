baa573e096d6f283c4f03c7f19b53219ee618e98
Nathan Chambers <nathan.m.chambers@gmail.com>
2019-03-02 16:47:41 +1100

    Boosted AGM-K and Javelin hit damage. Its over 9000!

--- core/socomd_core/configs/CfgAmmo.hpp
+++ core/socomd_core/configs/CfgAmmo.hpp
@@ -1,5 +1,7 @@
 class CfgAmmo {
 	class BulletBase;
+	class MissileBase;
+	class SmokeShell;
 
 	class B_9x21_Ball: BulletBase {	
 		hit = 8.5;
@@ -8,9 +10,23 @@ class CfgAmmo {
 	class HLC_300WM_FMJ: BulletBase {
 		hit = 25;
 	};
-	class SmokeShell;
+	
 	class G_40mm_Smoke: SmokeShell {
 		simulation = "shotSmoke";
 		deflectionSlowDown = 0;
+	};
+	
+	class M_Scalpel_AT : MissileBase {
+		//Empty
+	};
+
+	//Parents: "M_Scalpel_AT","MissileBase","MissileCore","Default"
+	class ACE_Hellfire_AGM114K : M_Scalpel_AT {
+		hit = 9001;
+	};
+
+	//Parents: "MissileBase","MissileCore","Default"
+	class CUP_M_Javelin_AT : MissileBase {
+		hit = 9001;
 	};
 };
