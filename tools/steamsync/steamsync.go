734ca60762d1147bbf7e6bf5528f62ea0634a3e8
Nathan Chambers <nathan.m.chambers@gmail.com>
2019-05-27 23:04:39 +1000

    force linux symlinks.

--- tools/steamsync/steamsync.go
+++ tools/steamsync/steamsync.go
@@ -102,7 +102,7 @@ func SymlinkContent(name string, id string) error {
 		cmdArgs = []string{"/c", "mklink", "/d", dest, source}
 	} else {
 		cmdName = "ln"
-		cmdArgs = []string{"-s", source, dest}
+		cmdArgs = []string{"-sf", source, dest}
 	}
 
 	fmt.Println(fmt.Sprintf("%s %s", cmdName, cmdArgs))
