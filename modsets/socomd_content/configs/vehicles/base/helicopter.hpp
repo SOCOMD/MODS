d03eaa29edd7dd982e26529ca9b2fecdd0de00d1
Anthony "Zanven" Poschen <zanven42@gmail.com>
2019-06-10 19:41:20 +1000

    Little Bird Health Balanced. 1x RPG, or 12 zu23 rounds now for full kill. was 350 rounds.

--- modsets/socomd_content/configs/vehicles/base/helicopter.hpp
+++ modsets/socomd_content/configs/vehicles/base/helicopter.hpp
@@ -1,5 +1,31 @@
-//class Helicopter_Base_H;
-//class MELB_base : Helicopter_Base_H {};
-class MELB_base;
-class MELB_MH6M : MELB_base { };
-class MELB_AH6M : MELB_base { };
+class Helicopter_Base_H;
+class MELB_base : Helicopter_Base_H {
+	class Hitpoints;
+};
+
+class MELB_MH6M : MELB_base {
+	class HitPoints : HitPoints {
+		class HitHull;
+		class HitEngine;
+		class HitFuel;
+		class HitAvionics;
+		class HitGlass1;
+		class HitHRotor;
+		class HitMissiles;
+		class HitTransmission;
+		class HitVRotor;
+	};
+ };
+class MELB_AH6M : MELB_base {
+	class HitPoints : HitPoints {
+		class HitHull;
+		class HitEngine;
+		class HitFuel;
+		class HitAvionics;
+		class HitGlass1;
+		class HitHRotor;
+		class HitMissiles;
+		class HitTransmission;
+		class HitVRotor;
+	};
+ };
--- modsets/socomd_content/configs/vehicles/override/firefly.hpp
+++ modsets/socomd_content/configs/vehicles/override/firefly.hpp
@@ -14,7 +14,71 @@ class SOCOMD_FIREFLY_A : MELB_MH6M {
 		"socomd_data_core\ah6\ah6_hull_co.paa",
 		"melb\data\decals\sn\blank_ca.paa"
 	};
-	
+	armor = 30;
+	armorStructural = 4;
+	class HitPoints : HitPoints {
+		class HitHull : HitHull {
+			material = -1;
+			armor = 1.0;
+			passThrough = 1.0;
+			explosionShielding = 1.0;
+		};
+		class HitEngine : HitEngine {
+			armor = 3.0;
+			passThrough = 1.0;
+			explosionShielding = 1.0;
+			minimalHit = 0.01;
+			material = -1;
+		};
+		class HitFuel : HitFuel {
+			armor = 3.0;
+			passThrough = 1.0;
+			explosionShielding = 1.0;
+			minimalHit = 0.01;
+			material = -1;
+		};
+		class HitAvionics : HitAvionics {
+			armor = 3.0;
+			passThrough = 1.0;
+			explosionShielding = 1.0;
+			material = -1;
+		};
+		class HitGlass1 : HitGlass1 {
+			armor = 1.0;
+			passThrough = 1.0;
+			explosionShielding = 0.0;
+			minimalHit = 0.1;
+			material = -1;
+		};
+		class HitHRotor : HitHRotor {
+			armor = 4.0;
+			passThrough = 0.0;
+			explosionShielding = 1.0;
+			minimalHit = 0.01;
+			material = -1;
+		};
+		class HitVRotor : HitVRotor {
+			armor = 1.0;
+			passThrough = 0.0;
+			explosionShielding = 5.0;
+			minimalHit = 0.01;
+			material = -1;
+		};
+		class HitMissiles : HitMissiles {
+			armor = 1.0;
+			passThrough = 1.0;
+			explosionShielding = 0.0;
+			minimalHit = 0.1;
+			material = -1;
+		};
+		class HitTransmission : HitTransmission {
+			armor = 3.0;
+			passThrough = 1.0;
+			explosionShielding = 1.0;
+			minimalHit = 0.01;
+			material = -1;
+		};
+	};
 };
 
 class SOCOMD_DRAGONFLY_A : MELB_AH6M {
@@ -31,5 +95,70 @@ class SOCOMD_DRAGONFLY_A : MELB_AH6M {
 	hiddenSelectionsTextures[] = {
 		"socomd_data_core\ah6\ah6_hull_co.paa",
 		"melb\data\decals\sn\blank_ca.paa"
+	};
+	armor = 30;
+	armorStructural = 4;
+	class HitPoints : HitPoints {
+		class HitHull : HitHull {
+			material = -1;
+			armor = 1.0;
+			passThrough = 1.0;
+			explosionShielding = 1.0;
+		};
+		class HitEngine : HitEngine {
+			armor = 3.0;
+			passThrough = 1.0;
+			explosionShielding = 1.0;
+			minimalHit = 0.01;
+			material = -1;
+		};
+		class HitFuel : HitFuel {
+			armor = 3.0;
+			passThrough = 1.0;
+			explosionShielding = 1.0;
+			minimalHit = 0.01;
+			material = -1;
+		};
+		class HitAvionics : HitAvionics {
+			armor = 3.0;
+			passThrough = 1.0;
+			explosionShielding = 1.0;
+			material = -1;
+		};
+		class HitGlass1 : HitGlass1 {
+			armor = 1.0;
+			passThrough = 1.0;
+			explosionShielding = 0.0;
+			minimalHit = 0.1;
+			material = -1;
+		};
+		class HitHRotor : HitHRotor {
+			armor = 4.0;
+			passThrough = 0.0;
+			explosionShielding = 1.0;
+			minimalHit = 0.01;
+			material = -1;
+		};
+		class HitVRotor : HitVRotor {
+			armor = 1.0;
+			passThrough = 0.0;
+			explosionShielding = 5.0;
+			minimalHit = 0.01;
+			material = -1;
+		};
+		class HitMissiles : HitMissiles {
+			armor = 1.0;
+			passThrough = 1.0;
+			explosionShielding = 0.0;
+			minimalHit = 0.1;
+			material = -1;
+		};
+		class HitTransmission : HitTransmission {
+			armor = 3.0;
+			passThrough = 1.0;
+			explosionShielding = 1.0;
+			minimalHit = 0.01;
+			material = -1;
+		};
 	};
 };
