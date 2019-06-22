6058d498d7ba1dd9c364f3f1e5612e3a89ffedbd
Daniel S <dansgeo@gmail.com>
2019-06-22 14:30:04 +1000

    Vehicle Selector + weapon light

--- modsets/socomd_content/configs/CfgASORVS.hpp
+++ modsets/socomd_content/configs/CfgASORVS.hpp
@@ -6,8 +6,10 @@ class CfgASORVS {
 
 	class Land {
 		SOCOMD_Content[] = {
-			"SOCOMD_SOV_HMG_D", 
-			"SOCOMD_SOV_HMG_W", 
+			"SOCOMD_SOV_HMG_D",
+			"SOCOMD_SOV_HMG_W",
+			"SOCOMD_SOV_GMG_D", 
+			"SOCOMD_SOV_GMG_W",
 			"SOCOMD_LR_ATV",
 			"V12_KTM_SOCOMD"
 		};
