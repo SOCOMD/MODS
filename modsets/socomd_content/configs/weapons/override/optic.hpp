a0ab0db5b62ce00b144bced7d13cc209243e98d2
Nathan Chambers <nathan.m.chambers@gmail.com>
2019-09-05 14:49:04 +1000

    Re-formatted optics file. Added compat for RH_ta31rmr

--- modsets/socomd_content/configs/weapons/override/optic.hpp
+++ modsets/socomd_content/configs/weapons/override/optic.hpp
@@ -9,6 +9,7 @@ distanceZoomMax = ZOOM_MAX; \
 #define OPTIC_HoloM OPTIC_FOV(0.75,0.25,0.5,100,100,false)
 #define OPTIC_T1 OPTIC_FOV(1.25,0.25,0.75,100,100,false)
 #define OPTIC_SMA_ELCAN OPTIC_FOV(0.75,0.25,0.4,100,100,false)
+#define OPTIC_RH_ACOG OPTIC_FOV(0.75,0.25,0.4,100,100,false)
 /*
 #define OPTIC_MarkM OPTIC_FOV(0.16,0.15,100,400,true)*/
 
@@ -30,87 +31,68 @@ class RH_t1_tan : RH_t1 {};
 
 class SMA_eotech: ItemCore {};
 
-class SMA_eotechG33_3XDOWN: SMA_eotech
-	{
-		class ItemInfo: InventoryOpticsItem_Base_F
-		{
-			class OpticsModes
-			{
-				class AIM
-				{
-					OPTIC_Holo
-				};
+class SMA_eotechG33_3XDOWN: SMA_eotech {
+	class ItemInfo: InventoryOpticsItem_Base_F {
+		class OpticsModes {
+			class AIM {
+				OPTIC_Holo
 			};
 		};
 	};
-class SMA_eotechG33_3XUP: SMA_eotechG33_3XDOWN
-	{
-		class ItemInfo: InventoryOpticsItem_Base_F
-		{
-			class OpticsModes
-			{
-				class Magnified
-				{
-					OPTIC_HoloM
-				};
+};
+
+class SMA_eotechG33_3XUP: SMA_eotechG33_3XDOWN {
+	class ItemInfo: InventoryOpticsItem_Base_F {
+		class OpticsModes {
+			class Magnified {
+				OPTIC_HoloM
 			};
 		};
 	};
-class SMA_eotechG33_grn_3XDOWN: SMA_eotech
-	{
-		class ItemInfo: InventoryOpticsItem_Base_F
-		{
-			class OpticsModes
-			{
-				class AIM
-				{
-					OPTIC_Holo
-				};
+};
+
+class SMA_eotechG33_grn_3XDOWN: SMA_eotech {
+	class ItemInfo: InventoryOpticsItem_Base_F {
+		class OpticsModes {
+			class AIM {
+				OPTIC_Holo
 			};
 		};
 	};
-class SMA_eotechG33_grn_3XUP: SMA_eotechG33_3XDOWN
-	{
-		class ItemInfo: InventoryOpticsItem_Base_F
-		{
-			class OpticsModes
-			{
-				class Magnified
-				{
-					OPTIC_HoloM
-				};
+};
+
+class SMA_eotechG33_grn_3XUP: SMA_eotechG33_3XDOWN {
+	class ItemInfo: InventoryOpticsItem_Base_F {
+		class OpticsModes {
+			class Magnified {
+				OPTIC_HoloM
 			};
 		};
 	};
-class SMA_eotechG33_tan_3XDOWN: SMA_eotech
-	{
-		class ItemInfo: InventoryOpticsItem_Base_F
-		{
-			class OpticsModes
-			{
-				class AIM
-				{
-					OPTIC_Holo
-				};
+};
+
+class SMA_eotechG33_tan_3XDOWN: SMA_eotech {
+	class ItemInfo: InventoryOpticsItem_Base_F {
+		class OpticsModes {
+			class AIM {
+				OPTIC_Holo
 			};
 		};
 	};
-class SMA_eotechG33_tan_3XUP: SMA_eotechG33_3XDOWN
-	{
-		class ItemInfo: InventoryOpticsItem_Base_F
-		{
-			class OpticsModes
-			{
-				class Magnified
-				{
-					OPTIC_HoloM
-				};
+};
+
+class SMA_eotechG33_tan_3XUP: SMA_eotechG33_3XDOWN {
+	class ItemInfo: InventoryOpticsItem_Base_F {
+		class OpticsModes {
+			class Magnified {
+				OPTIC_HoloM
 			};
 		};
 	};
+};
 
 class SMA_ELCAN_SPECTER : ItemCore {
-	scope = 1;
+	scope = protected;
 	class ItemInfo : InventoryOpticsItem_Base_F {
 		class OpticsModes {
 			class ACOG4x {
@@ -128,11 +110,11 @@ class SMA_ELCAN_SPECTER : ItemCore {
 };
 
 class SMA_ELCAN_SPECTER5: SMA_ELCAN_SPECTER {
-	scope=2;
+	scope=public;
 };
 
 class SMA_ELCAN_SPECTER_GREEN : ItemCore {
-	scope=1;
+	scope=protected;
 	class ItemInfo : InventoryOpticsItem_Base_F {
 		class OpticsModes {
 			class ACOG4x {
@@ -150,11 +132,11 @@ class SMA_ELCAN_SPECTER_GREEN : ItemCore {
 };
 
 class SMA_ELCAN_SPECTER_GREEN5: SMA_ELCAN_SPECTER_GREEN {
-	scope=2;
+	scope=public;
 };
 
 class SMA_ELCAN_SPECTER_TAN : ItemCore {
-	scope=1;
+	scope=protected;
 	class ItemInfo : InventoryOpticsItem_Base_F {
 		class OpticsModes {
 			class ACOG4x {
@@ -172,5 +154,21 @@ class SMA_ELCAN_SPECTER_TAN : ItemCore {
 };
 
 class SMA_ELCAN_SPECTER_TAN5: SMA_ELCAN_SPECTER_TAN {
-	scope=2;
+	scope=public;
+};
+
+class RH_ta31rmr : ItemCore {
+	scope=public;
+	class ItemInfo : InventoryOpticsItem_Base_F {
+		class OpticsModes {
+			class acogcqb {
+				OPTIC_RH_ACOG
+				memoryPointCamera = "opticView";
+			};
+
+			class acogscope {
+				OPTIC_RH_ACOG
+			};
+		};
+	};
 };
