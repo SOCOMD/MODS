c6b318271ede26cadcd0cf0c5015f080e828c81e
Samuel Glew <samuel.glew@gmail.com>
2019-09-05 20:08:07 +0930

    adding volume reduction/protection to all ADFU helmets currently used

--- core/socomd_core/configs/weapons/base/headgear_hard.hpp
+++ core/socomd_core/configs/weapons/base/headgear_hard.hpp
@@ -12,6 +12,13 @@ class ItemInfo : ItemInfo { \
 #define HELMET_ARMOR_STRONG HELMET_ARMOR(12,0.1)
 #define HELMET_SOLS_ARMOR HELMET_ARMOR(12,0.1)
 
+#define HEARING_PROTECTION_VICCREW ace_hearing_lowerVolume = 0.85;\
+ace_hearing_protection = 0.6;
+#define HEARING_PROTECTION_EARMUFF ace_hearing_lowerVolume = 0.75;\
+ace_hearing_protection = 0.5;
+#define HEARING_PROTECTION_PELTOR ace_hearing_lowerVolume = 0.55;\
+ace_hearing_protection = 0.4;
+
 ////////////////////////////////////////////////////////////////////////////////
 
 class HeadgearItem;
@@ -31,12 +38,23 @@ class ADFU_H_OpsCore_02 : ItemCore {
 	class ItemInfo;
 };
 
+class ADFU_H_OpsCore_03 : ItemCore {
+	class ItemInfo;
+	HEARING_PROTECTION_PELTOR
+}
 class ADFU_H_OpsCore_08 : ItemCore {
 	class ItemInfo;
+	HEARING_PROTECTION_PELTOR
 };
 
 class ADFU_H_Airframe_Cover_03 : ItemCore {
 	class ItemInfo;
+	HEARING_PROTECTION_PELTOR
+};
+
+class ADFU_H_Airframe_Cover_10 : ItemCore {
+	class ItemInfo;
+	HEARING_PROTECTION_PELTOR
 };
 
 class ADFU_H_Airframe_Cover_03_MC;
@@ -467,7 +485,7 @@ class ADFU_H_Airframe_11_OD: ADFU_H_Airframe_Cover_05 {
 	};
 };
 
-class ADFU_H_Airframe_AMCU: ADFU_H_Airframe_Cover_05 {
+class ADFU_H_Airframe_11_AMCU: ADFU_H_Airframe_Cover_05 {
 	scope = public;
 	scopeArsenal = 2;
 	displayName="Airframe (AMCU)";
@@ -489,6 +507,39 @@ class ADFU_H_Airframe_AMCU: ADFU_H_Airframe_Cover_05 {
 
 	hiddenSelectionsTextures[]= {
 		"",
+		"ADFU_Units\data\h_gear_01_khk_co.paa",
+		"socomd_data_core\CTgear\h_airframe_cover_AMC_co.paa",
+		"",
+		"",
+		"ADFU_Units\data\h_gear_01_khk_co.paa",
+		"ADFU_Units\data\h_gear_01_khk_co.paa",
+		"socomd_data_core\CTgear\h_airframe_OD_co.paa",
+		"socomd_data_core\CTgear\h_fast_khk_co.paa"
+	};
+};
+
+class ADFU_H_Airframe_10_AMCU: ADFU_H_Airframe_Cover_03 {
+	scope = public;
+	scopeArsenal = 2;
+	displayName="Airframe (AMCU)";
+	picture="\ADFU_Units\ui\icon_h_airframe_cb_ca.paa";
+
+	HELMET_ARMOR_STRONG
+
+	hiddenSelections[]= {
+		"_arc_peltor",
+		"_camera",
+		"_cover",
+		"_face_cover",
+		"_Manta_Rear",
+		"_NVG_Battery",
+		"_surefire_light",
+		"camo",
+		"camo2"
+	};
+
+	hiddenSelectionsTextures[]= {
+		"ADFU_Units\data\h_gear_01_khk_co.paa",
 		"ADFU_Units\data\h_gear_01_khk_co.paa",
 		"socomd_data_core\CTgear\h_airframe_cover_AMC_co.paa",
 		"",
