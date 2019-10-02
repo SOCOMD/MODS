60a6ed8669c2a1454ee81abecc64f45345e855e6
Daniel S <dansgeo@gmail.com>
2019-10-02 11:25:52 +1000

    Revert "fix dans woops"
    This reverts commit b50ccb8fba3a8035c91ae54939782be987d6c296.

---
+++ modsets/socomd_content/configs/CfgFunctions.hpp
@@ -0,0 +1,19 @@
+class CfgFunctions {
+	class SOCOMD {
+		class EXH {
+			file="\socomd_content\functions\eventHandlers";
+			class Event_rpgMalfunction {};
+			class Event_weaponMalfunction {};
+		};
+	class bnae_functions_f {
+		tag="bnae";
+			class Misc
+			{
+				file="\socomd_core\functions\bnae_functions";
+				class inspect
+				{
+				};
+			};
+		};
+	};
+};
