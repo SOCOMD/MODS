440cfe28c233cd4317aea1844f9ead5c4ad0a2ac
Daniel S <dansgeo@gmail.com>
2019-10-17 22:47:58 +1000

    SSA

--- modsets/socomd_content/configs/weapons/base/vest.hpp
+++ modsets/socomd_content/configs/weapons/base/vest.hpp
@@ -10,40 +10,4 @@ class SOCOMD_RUS_Vest: CUP_V_B_LBT_LBV_MCam
 		displayName="Russian Special Forces Vest";
 		author="Monk";
 		descriptionShort="Vest used by Russian SF";
-		class ItemInfo: VestItem
-		{
-			containerClass="Supply150";
-			mass=20;
-			hiddenSelections[]=
-			{
-				"Camo",
-				"Insignia"
-			};
-			class HitpointsProtectionInfo
-			{
-				class Chest
-				{
-					HitpointName="HitChest";
-					armor=40;
-					PassThrough=0.1;
-				};
-				class Diaphragm
-				{
-					HitpointName="HitDiaphragm";
-					armor=40;
-					PassThrough=0.1;
-				};
-				class Abdomen
-				{
-					hitpointName="HitAbdomen";
-					armor=40;
-					passThrough=0.1;
-				};
-				class Body
-				{
-					hitpointName="HitBody";
-					passThrough=0.1;
-				};
-			};
-		};
 	};
