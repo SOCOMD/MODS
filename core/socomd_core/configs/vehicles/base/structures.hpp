b5a524ff3855095a0a7a3f4a66ee4d1fd8855dfc
Anthony "Zanven" Poschen <zanven42@gmail.com>
2020-02-16 17:47:34 +1100

    Ensure Ammo Facility at FOB is setup in config, removed all custom attributes from FOB's.

--- core/socomd_core/configs/vehicles/base/structures.hpp
+++ core/socomd_core/configs/vehicles/base/structures.hpp
@@ -3,3 +3,14 @@ class Building : Static {
     // this appears to do nothing
     // damageResistance=4000; // default 0.004;
 };
+
+// FOB Ammo Structure
+class Slingload_01_Base_F;
+class B_Slingload_01_Ammo_F : Slingload_01_Base_F {
+    ace_rearm_isSupplyVehicle = 1;
+    ace_rearm_defaultSupply = 1000000;
+    ace_cargo_hasCargo = 0;
+    ace_cargo_space = 0;
+};
+
+// ace_repair_canRepair
