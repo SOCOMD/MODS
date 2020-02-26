0ea67839ec6ba78536cd11298aadfa88688aca2c
Daniel S <dansgeo@gmail.com>
2020-02-26 21:38:23 +1000

    AMCU face wrap + USP9mm

--- modsets/socomd_content/configs/magazines/override/socomd_men_magazines.hpp
+++ modsets/socomd_content/configs/magazines/override/socomd_men_magazines.hpp
@@ -5,3 +5,4 @@ class SOCOMD_Item_Magazine_762x51_150Rnd_Tracer     : CUP_150Rnd_TE4_LRT4_White_
 class SOCOMD_Item_Magazine_300_5Rnd                 : hlc_5rnd_300WM_FMJ_AWM {scope = protected;};
 class SOCOMD_Item_Magazine_Barret                   : Recon_Barret_mag {scope = protected;};
 class SOCOMD_Item_Magazine_9x21_60Rnd               : CUP_30Rnd_9x19_MP5 {};
+class SOCOMD_Item_Magazine_45ACP_11Rnd              : 18Rnd_9x21_Mag_USP {scope = protected;};
