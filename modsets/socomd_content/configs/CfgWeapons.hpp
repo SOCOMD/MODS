462444c19b160a7bc74c9414f8d39a410b5d0f49
Daniel S <dansgeo@gmail.com>
2020-03-19 14:24:06 +1000

    LASER POINTER

--- modsets/socomd_content/configs/CfgWeapons.hpp
+++ modsets/socomd_content/configs/CfgWeapons.hpp
@@ -13,6 +13,7 @@ class CfgWeapons {
 
 	//Item Defines
 	#include "weapons\base\primary.hpp"
+	#include "weapons\base\pointer.hpp"
 	#include "weapons\base\secondary.hpp"
 	#include "weapons\base\handgun.hpp"
 	#include "weapons\base\uniform.hpp"
