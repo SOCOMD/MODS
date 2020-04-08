3dd9ba595e9de0a472e9bfb2aa19f7310d30efdf
Nathan Chambers <nathan.m.chambers@gmail.com>
2020-04-08 15:32:35 +1000

    Added MX to weapon loadout selection.

--- core/socomd_core/configs/magazines/override/socomd_men_magazines.hpp
+++ core/socomd_core/configs/magazines/override/socomd_men_magazines.hpp
@@ -2,8 +2,8 @@
 class SOCOMD_Item_Magazine_556x45_30Rnd             : 30Rnd_556x45_Stanag_Red {scope = protected;};
 class SOCOMD_Item_Magazine_556x45_30Rnd_Tracer      : 30Rnd_556x45_Stanag_Tracer_Red {scope = protected;};
 class SOCOMD_Item_Magazine_556x45_200Rnd_Tracer     : CUP_200Rnd_TE4_Red_Tracer_556x45_M249_Pouch {scope = protected;};
-class SOCOMD_Item_Magazine_65x39_30Rnd              : 30Rnd_65x39_caseless_mag {scope = protected;};
-class SOCOMD_Item_Magazine_65x39_30Rnd_Tracer       : 30Rnd_65x39_caseless_mag_Tracer {scope = protected;};
+class SOCOMD_Item_Magazine_65x39_30Rnd              : 30Rnd_65x39_caseless_black_mag {scope = protected;};
+class SOCOMD_Item_Magazine_65x39_30Rnd_Tracer       : 30Rnd_65x39_caseless_black_mag_Tracer {scope = protected;};
 class SOCOMD_Item_Magazine_762x51_20Rnd             : 20Rnd_762x51_Mag {scope = protected;};
 class SOCOMD_Item_Magazine_762x51_150Rnd_Tracer     : 150Rnd_762x54_Box_Tracer {scope = protected;};
 class SOCOMD_Item_Magazine_408_7Rnd                 : 7Rnd_408_Mag {scope = protected;};
