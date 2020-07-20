8a6e9bf9dd113070d315f03b87b9b9c9c364be33
Samuel Glew <samuel.glew@gmail.com>
2020-07-20 20:53:26 +0930

    allowing other people to load the gustav if they have the ammo

--- modsets/socomd_content/configs/weapons/base/secondary.hpp
+++ modsets/socomd_content/configs/weapons/base/secondary.hpp
@@ -3,5 +3,6 @@ class tf47_m3maaws; //MAT
 class SOCOMD_Carl_Gustav : tf47_m3maaws {
 	author = AUTHOR_STR;
 	displayName = "Carl Gustav";
+	ace_reloadlaunchers_enabled = 1;
 };
 class CUP_launch_Javelin; //HAT
