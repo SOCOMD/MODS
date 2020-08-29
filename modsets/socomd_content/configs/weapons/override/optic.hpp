9025d369b327c0ba7f5f05ceb0a5475cf22b43b2
Samuel Glew <samuel.glew@gmail.com>
2020-08-29 15:55:32 +0930

    updating monk's G33 scopes to be accepted by the zoom

--- modsets/socomd_content/configs/weapons/override/optic.hpp
+++ modsets/socomd_content/configs/weapons/override/optic.hpp
@@ -296,3 +296,59 @@ class CUP_optic_ACOG_TA01B_RMR_Tropic_3D: CUP_optic_ACOG_TA01B_RMR_Black_3D{
 	delete MRT_SwitchItemNextClass;
 	delete MRT_SwitchItemPrevClass;
 };
+class acwp_t2;
+class acwp_t2_g33: acwp_t2 {
+	MRT_SwitchItemNextClass = "acwp_t2_g33_down";
+    MRT_SwitchItemPrevClass = "acwp_t2_g33_down";
+    MRT_switchItemHintText = "Up";
+	scope = 1;
+	displayname = "Micro T2 G33";
+	model = "M4A5_Troy_Acc\mesh\t2\t2_g33.p3d";
+		class ItemInfo : InventoryOpticsItem_Base_F {
+	    	mass = 4;
+	    	modelOptics = "\A3\Weapons_F\empty";
+	    	optics = 0;
+	    	class OpticsModes {
+	    	class ACO {
+	    	    opticsID = 1;
+				useModelOptics = 0;
+				opticsPPEffects[] = {"OpticsCHAbera5","OpticsBlur5"};
+				opticsFlare = 1;
+				opticsDisablePeripherialVision = 1;
+				memoryPointCamera = "RDeye";
+				discretedistance[] = {200};
+				discreteDistanceInitIndex = 1;
+				visionMode[] = {};
+				OPTIC_4X_OPTIC
+	    	};
+	    };
+ 	};
+};
+class acwp_eotech;
+class acwp_eotech_g33: acwp_eotech {
+	MRT_SwitchItemNextClass = "acwp_eotech_g33_down";
+    MRT_SwitchItemPrevClass = "acwp_eotech_g33_down";
+    MRT_switchItemHintText = "Up";
+	scope = 1;
+	displayname = "EOTech EXPS3 G33";
+	model = "M4A5_Troy_Acc\mesh\exps\exps_g33.p3d";
+		class ItemInfo : InventoryOpticsItem_Base_F {
+	    	mass = 4;
+	    	modelOptics = "\A3\Weapons_F\empty";
+	    	optics = 0;
+	    	class OpticsModes {
+	    	class ACO {
+	    	    opticsID = 1;
+				useModelOptics = 0;
+				opticsPPEffects[] = {"OpticsCHAbera5","OpticsBlur5"};
+				opticsFlare = 1;
+				opticsDisablePeripherialVision = 1;
+				memoryPointCamera = "RDeye";
+				discretedistance[] = {200};
+				discreteDistanceInitIndex = 1;
+				visionMode[] = {};
+				OPTIC_4X_OPTIC
+	    	};
+	    };
+ 	};
+};
