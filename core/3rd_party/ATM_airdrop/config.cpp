5d1c233776475ed33129e4a283c733843e8f98dd
Samuel Glew <samuel.glew@gmail.com>
2021-03-06 14:32:13 +1030

    adding Airdrop script to the repo

---
+++ core/3rd_party/ATM_airdrop/config.cpp
@@ -0,0 +1,20 @@
+#include "BIS_AddonInfo.hpp"
+class CfgPatches
+{
+	class ATM_airdrop
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 1.0;
+		requiredAddons[] = {};
+		author[] = {};
+		authorUrl = "";
+		version = 0.0.1;
+		versionStr = "0.0.1";
+		versionAr[] = { 0,0,1 };
+	};
+};
+
+#include "\ATM_airdrop\CfgFunctions.hpp"
+#include "\ATM_airdrop\CfgSounds.hpp"
+#include "\ATM_airdrop\dialog.hpp"
