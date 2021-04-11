57d3097e5e21d838dc9fec615f65dad6d288e76a
Samuel Glew <samuel.glew@gmail.com>
2021-04-11 21:27:43 +0930

    missed eol

--- core/socomd_medical/configs/weapons/Resuscitation.hpp
+++ core/socomd_medical/configs/weapons/Resuscitation.hpp
@@ -10,12 +10,12 @@ class kat_chestSeal: ACE_ItemCore {
 class kat_larynx: ACE_ItemCore {
 	displayName="i-gel SGA";
 	picture="\socomd_medical\tex\LMA.paa";
-	descriptionShort = "i-gel supraglottic airway device made by Intersurgical. sed to keep structure to the airway while preventing further blockage"
-	descriptionUse = "i-gel supraglottic airway device made by Intersurgical. sed to keep structure to the airway while preventing further blockage"
+	descriptionShort = "i-gel supraglottic airway device made by Intersurgical. sed to keep structure to the airway while preventing further blockage";
+	descriptionUse = "i-gel supraglottic airway device made by Intersurgical. sed to keep structure to the airway while preventing further blockage";
 };
 class kat_guedel: ACE_ItemCore {
 	displayName="Nasopharyngeal Airway with Lubricant";
 	picture="\socomd_medical\tex\NPA.paa";
-	descriptionShort = "Nasopharyngeal Airway made by Rusch"
-	descriptionUse
+	descriptionShort = "Nasopharyngeal Airway made by Rusch";
+	descriptionUse = "Nasopharyngeal Airway made by Rusch";
 };
