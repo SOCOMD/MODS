1146e942dcf59e4b728ace25fee60941c2cbd620
Slatery <samuel.glew@gmail.com>
2021-09-13 12:55:24 +0930

    Update cleanup.py
    correcting cleanup tool

--- misc/socomd_missions/tools/cleanup.py
+++ misc/socomd_missions/tools/cleanup.py
@@ -7,7 +7,7 @@ path_parent = os.path.dirname(os.getcwd())
 
 
 for subdir, dirs, files in os.walk(path_parent + "\\fobs"):
-    for file in subdir:
+    for file in files:
         if not file.endswith(".sqm"): 
             print("removing: " + os.path.join(subdir, file))
             os.remove( os.path.join(subdir, file))
