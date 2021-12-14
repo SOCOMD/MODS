5d1114a659dee59ce8d1980ea1e3ffce769002a0
Samuel Glew <samuel.glew@gmail.com>
2021-12-14 12:18:54 +1030

    fixing missing base classes

--- addons/vehicles_land/configs/vehicles/base_classes.hpp
+++ addons/vehicles_land/configs/vehicles/base_classes.hpp
@@ -1,23 +1,56 @@
-// Declarations to allow overwrites
 class Land;
 
-class LandVehicle;
-class Car;
-class Car_F; 
 
+class LandVehicle : Land {
+    class Components;
+};
+
+class Car : LandVehicle {
+    class NewTurret;
+    class Components : Components {
+        class AICarSteeringComponent;
+    };
+};
+
+class Car_F : Car {
+    class HitPoints;
+    class textureSources;
+    
+    class Turrets {
+        class CargoTurret;
+        class MainTurret : NewTurret {};
+    };
+    class Components : Components {
+       
+    };
+};
+    // shared taki HMG
 class StaticWeapon;
-class StaticMGWeapon; 
-class StaticMortar;
+class StaticMGWeapon : StaticWeapon {
+    class Turrets;
+};
 
-class Wheeled_APC_F;
+class Wheeled_APC_F : Car_F {};
 
-class Tank;
+class Tank : LandVehicle {
+    class Turrets;
+};
 
-class Tank_F;
+class Tank_F : Tank {
+    class Turrets : Turrets {
+        class MainTurret;
+    };
+    class Components;
+};
 
-class APC_Tracked_02_base_F;
-
-
+// Predefines to allow overwrites
 class Truck_F;
 
-class StaticCannon;
+
+
+///////////////////////////////////////////////////////////////////////////////////
+// ZU23 Turrets
+class StaticCannon : StaticWeapon {
+    class Turrets;
+    class Components;
+};
