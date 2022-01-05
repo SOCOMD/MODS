64c210476790e51f550ef43b2229e8eb88d8123e
ToxicSpare <73157862+ToxicSpare@users.noreply.github.com>
2022-01-05 21:11:00 +1100

    Add afp qstore

--- addons/qstore/configs/CfgVehicles.hpp
+++ addons/qstore/configs/CfgVehicles.hpp
@@ -2,4 +2,5 @@ class CfgVehicles {
     #include "vehicles\boxes\socomd.hpp"
     #include "vehicles\boxes\rus.hpp"
     #include "vehicles\boxes\lowvis.hpp"
+    #include "vehicles\boxes\afp.hpp"
 };
