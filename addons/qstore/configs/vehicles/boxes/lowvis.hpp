a2c621b521d1540491e71bbb7474ec717ec0552f
SpectreOfRohan <rohanthepro@gmail.com>
2022-01-15 12:39:59 +1000

    nothing of note

--- addons/qstore/configs/vehicles/boxes/lowvis.hpp
+++ addons/qstore/configs/vehicles/boxes/lowvis.hpp
@@ -24,7 +24,7 @@ class SOCOMD_QStore_LOWVIS : SOCOMD_QStore_Base {
                 QSTORE_ACTION_GRP_BEGIN(SELECT_LOADOUTS_SUB0,"Change Role (LOWVIS)")
                     QSTORE_ACTION_LOADOUT("Commander",SOCOMD_LOWVIS_Commander)
                     QSTORE_ACTION_LOADOUT("Leader",SOCOMD_LOWVIS_Leader)
-                    QSTORE_ACTION_LOADOUT("2IC",SOCOMD_LOWVIS_2IC)
+                    //QSTORE_ACTION_LOADOUT("2IC",SOCOMD_LOWVIS_2IC)
                     QSTORE_ACTION_LOADOUT("Rifleman",SOCOMD_LOWVIS_Rifleman)
                     QSTORE_ACTION_LOADOUT("Marksman",SOCOMD_LOWVIS_Marksman)
                     QSTORE_ACTION_LOADOUT("Sapper",SOCOMD_LOWVIS_Sapper)
@@ -36,11 +36,12 @@ class SOCOMD_QStore_LOWVIS : SOCOMD_QStore_Base {
 
                 QSTORE_ACTION_GRP_BEGIN(SELECT_LOADOUTS_SUPPORT,"Change Role (SUPPORT)")
                     QSTORE_ACTION_LOADOUT("Logisitican",SOCOMD_LOWVIS_Logistician)
-                    QSTORE_ACTION_LOADOUT("Aeromedical Operator",SOCOMD_LOWVIS_AO)
+                    //QSTORE_ACTION_LOADOUT("Aeromedical Operator",SOCOMD_LOWVIS_AO)
                     QSTORE_ACTION_LOADOUT("Pilot",SOCOMD_LOWVIS_Pilot)
                     QSTORE_ACTION_LOADOUT("Crewman",SOCOMD_LOWVIS_Crewman)
                     QSTORE_ACTION_LOADOUT("Recon",SOCOMD_LOWVIS_Recon)
-                    QSTORE_ACTION_LOADOUT("Recon Asst.",SOCOMD_LOWVIS_Recon_Assistant)
+                    //QSTORE_ACTION_LOADOUT("HAVOC",SOCOMD_LOWVIS_TACP)
+                    //QSTORE_ACTION_LOADOUT("Recon Asst.",SOCOMD_LOWVIS_Recon_Assistant)
                 QSTORE_ACTION_GRP_END
 
             QSTORE_ACTION_GRP_END
