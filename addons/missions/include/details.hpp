e063cdf6b0fb1c0c5bafcf56620fb021a1d379a8
SpectreOfRohan <rohanthepro@gmail.com>
2022-01-16 16:50:01 +1000

    Update details.hpp

--- addons/missions/include/details.hpp
+++ addons/missions/include/details.hpp
@@ -16,7 +16,9 @@ class Header {
 // Information when selecting mission. Example picture: https://i.imgur.com/gug6ywM.jpg
 briefingName = "FOB Base Camp";    //Edit this to your unique mission name
 overviewPicture = "images\SOCOMD_Logo_transparent.paa";    //Take a picture of your FOB > save as PAA using Gruppe Addler .PAA convert > Put this image in the "images" folder of your mission > change name here to reflect.
-overviewText = "SOCOMD Mission<br /> 2CDO: Alpha Company, Phantom and Raider Platoons with Support Detachment.<br /><br /> 6 Avn: 171st Avn Sqn, Support Sqn and 4SQN Attachment.<br /><br />";
+overviewText = "SOCOMD Mission<br />\
+ 2CDO Alpha Company, Phantom and Raider Platoons with Support Detachment.<br />\
+ 6 Avn 171st Avn Sqn, Support Sqn and 4SQN Attachment.";
 
 // Information when loading into mission
 OnLoadName = "SOCOMD Mission";
